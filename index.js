8232d002c19223d157a6fc44cb2ef0953253c8ca
Tran Ngoc Hung <tranngochung1302@gmail.com>
2020-02-19 16:03:50 +0700

    aaa

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 import {AppRegistry} from 'react-native';
 // import register from './src/register';
 // import A from './src/components/A';
-import Test from './src/components/registration/Test';
-import FlatList from './src/components/FlatList/ExFlatList';
+import Test from './src/components/registration/test';
+// import FlatList from './src/components/FlatList/ExFlatList';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => FlatList);
+AppRegistry.registerComponent(appName, () => Test);
